1ba64092e68a97afe4f6a3cfbaa4b2bb9c3ae0b4
Aleksey <mr.wert2002@gmail.com>
2020-07-23 11:41:04 +0300

    init empty project

---
+++ src/main/java/io/moren/springkanban/SpringKanbanApplication.java
@@ -0,0 +1,13 @@
+package io.moren.springkanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKanbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKanbanApplication.class, args);
+    }
+
+}
