d9ac893314638d4a0247535a0729235d0ecd0a7a
Aleksey <mr.wert2002@gmail.com>
2020-07-23 15:25:15 +0300

    add jwt security

---
+++ src/main/java/io/moren/springkanban/security/JwtProperties.java
@@ -0,0 +1,7 @@
+package io.moren.springkanban.security;
+
+public final class JwtProperties {
+
+    public static final String HEADER = "Authorization";
+    public static final String PREFIX = "Bearer ";
+}
---
+++ src/main/java/io/moren/springkanban/security/JwtUtil.java
@@ -0,0 +1,43 @@
+package io.moren.springkanban.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.moren.springkanban.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+    private Long expired;
+
+    public String generateToken(Authentication authentication) {
+        User principal = (User) authentication.getPrincipal();
+
+        return Jwts
+                .builder()
+                .setSubject(principal.getUsername())
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String getUsername(String token) {
+
+        return Jwts
+                .parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(
+                        token.substring(JwtProperties.PREFIX.length())
+                )
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean resurveToken(String token) {
+        return false;
+    }
+
+}
