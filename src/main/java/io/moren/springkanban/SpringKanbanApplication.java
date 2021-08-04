7b3fc2c477cefe009f14c1419c11ff22839b9db0
alexey <morenaleksey@gmail.com>
2021-08-04 10:55:14 +0300

    remove data.sql

--- src/main/java/io/moren/springkanban/SpringKanbanApplication.java
+++ src/main/java/io/moren/springkanban/SpringKanbanApplication.java
@@ -1,13 +1,33 @@
 package io.moren.springkanban;
 
+import io.moren.springkanban.model.Role;
+import io.moren.springkanban.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringKanbanApplication {
+public class SpringKanbanApplication implements ApplicationRunner {
+
+    private RoleRepository roleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKanbanApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (roleRepository.findAll().isEmpty()) {
+            Role user = new Role();
+            user.setName("ROLE_USER");
+            roleRepository.save(user);
+        }
+    }
+
+    @Autowired
+    public void setRoleRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into roles values (1, 'ROLE_ADMIN') ON CONFLICT ON CONSTRAINT roles_pkey DO NOTHING;
-insert into roles values (2, 'ROLE_USER') ON CONFLICT ON CONSTRAINT roles_pkey DO NOTHING;
